dc8f96a2bf9a0efcdaea9a264c532916d1382cbe
jwbean <jwbean18@gmail.com>
2022-03-03 23:46:27 -0600

    Added scale display screen with some logic to draw a staff with notes

---
+++ SeniorProject/ScaleScreen.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace SeniorProject
+{
+    partial class ScaleScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.musicPanel = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.musicPanel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // musicPanel
+            // 
+            this.musicPanel.Location = new System.Drawing.Point(135, 136);
+            this.musicPanel.Name = "musicPanel";
+            this.musicPanel.Size = new System.Drawing.Size(494, 99);
+            this.musicPanel.TabIndex = 0;
+            this.musicPanel.TabStop = false;
+            this.musicPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.musicPanel_Paint);
+
+            // 
+            // ScaleScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.musicPanel);
+            this.Name = "ScaleScreen";
+            this.Text = "ScaleScreen";
+            ((System.ComponentModel.ISupportInitialize)(this.musicPanel)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox musicPanel;
+    }
+}
---
+++ SeniorProject/ScaleScreen.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeniorProject
+{
+    public partial class ScaleScreen : Form
+    {
+        public ScaleScreen()
+        {
+            InitializeComponent();
+        }
+
+        //https://stackoverflow.com/questions/4052598/draw-a-music-staff-in-c-sharp
+
+        private int _staffHght = 15;
+        private int _noteHght = 12;
+        private int _noteWdth = 20;
+        private Pen _notePen = new Pen(Color.Black, 2);
+        private Brush _noteBrush = Brushes.Black;
+
+        private void musicPanel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.HighQuality;
+
+            // draw some staff lines
+            for (int i = 1; i < 6; i++)
+                g.DrawLine(Pens.Black, 0, i * _staffHght, musicPanel.Width, i * _staffHght);
+
+            // draw four semi-random full and quarter notes
+            g.DrawEllipse(_notePen, 10, 2 * _staffHght, _noteWdth, _noteHght);
+            g.DrawEllipse(_notePen, 50, 4 * _staffHght, _noteWdth, _noteHght);
+            g.DrawEllipse(_notePen, 10, 3.6f * _staffHght, _noteWdth, _noteHght);
+
+            g.FillEllipse(_noteBrush, 100, 2 * _staffHght, _noteWdth, _noteHght);
+            g.FillEllipse(_noteBrush, 150, 4 * _staffHght, _noteWdth, _noteHght);
+        }
+    }
+}
