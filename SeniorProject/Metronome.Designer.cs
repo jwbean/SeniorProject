e256a459fe5995b89cddfc0dfba37804dbe8835c
jwbean <jwbean18@gmail.com>
2022-05-03 14:51:58 -0500

    Basic function of metronome made

---
+++ SeniorProject/Metronome.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace SeniorProject
+{
+    partial class Metronome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tempoBar = new System.Windows.Forms.TrackBar();
+            this.tempoSelect = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tempoBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tempoSelect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tempoBar
+            // 
+            this.tempoBar.Location = new System.Drawing.Point(260, 200);
+            this.tempoBar.Maximum = 200;
+            this.tempoBar.Minimum = 30;
+            this.tempoBar.Name = "tempoBar";
+            this.tempoBar.Size = new System.Drawing.Size(285, 45);
+            this.tempoBar.TabIndex = 1;
+            this.tempoBar.TickFrequency = 10;
+            this.tempoBar.Value = 80;
+            // 
+            // tempoSelect
+            // 
+            this.tempoSelect.Location = new System.Drawing.Point(366, 159);
+            this.tempoSelect.Maximum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.tempoSelect.Minimum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.tempoSelect.Name = "tempoSelect";
+            this.tempoSelect.Size = new System.Drawing.Size(47, 20);
+            this.tempoSelect.TabIndex = 2;
+            this.tempoSelect.Value = new decimal(new int[] {
+            80,
+            0,
+            0,
+            0});
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(479, 264);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Metronome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tempoSelect);
+            this.Controls.Add(this.tempoBar);
+            this.Controls.Add(this.button1);
+            this.Name = "Metronome";
+            this.Text = "Tuner";
+            ((System.ComponentModel.ISupportInitialize)(this.tempoBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tempoSelect)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar tempoBar;
+        private System.Windows.Forms.NumericUpDown tempoSelect;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ SeniorProject/Metronome.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeniorProject
+{
+    public partial class Metronome : Form
+    {
+        private bool buttonClicked = false;
+        
+        public Metronome()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (!buttonClicked)
+            {
+                buttonClicked = true;
+                var asmbly = Assembly.GetExecutingAssembly();
+                var wav = asmbly.GetManifestResourceStream("SeniorProject.InstrumentWavFiles.MetronomeClick.wav");
+                using (System.IO.Stream str = wav)
+                {
+                    decimal bpm = tempoSelect.Value;
+                    decimal millis = (1 / (bpm / 60)) * 1000;
+                    SoundPlayer player = new SoundPlayer(str);
+                    while (buttonClicked)
+                    {
+                        player.Load();
+                        player.Play();
+                        await Task.Delay((int) millis);
+                    }
+                }
+            }
+            else
+            {
+                buttonClicked = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            buttonClicked = false;
+        }
+    }
+}
